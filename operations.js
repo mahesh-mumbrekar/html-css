43d81fae0e7f16568e060d20e4b87e549a85f743
kwingston <mahesh.mummbrekar@gmail.com>
2023-05-30 20:33:10 +0530

    updated opearations.js by crating function of addition for math opeations

--- operations.js
+++ operations.js
@@ -1,5 +1,16 @@
-console.log(1 + 2)
-console.log(2 - 5)
-console.log(2 * 5)
-console.log(10 / 5)
-console.log(10 % 4)
+console.log(1 + 2);
+console.log(2 - 5);
+console.log(2 * 5);
+console.log(10 / 5);
+console.log(10 % 4);
+
+
+console.log('my ' + 'name ' + 'is ' + 'md');
+
+function addition(value1, value2) {
+    return value1+value2
+
+}
+
+let add = addition(50, 20); 
+console.log(add)
